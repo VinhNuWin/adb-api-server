6ade0026e9e65894c33052f697f0fd4f30ecfff6
Vinh Nguyen <VinhNuwin@protonmail.com>
2024-05-12 12:51:03 -0700

    changed app.listen to server

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const { exec } = require("child_process");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use(bodyParser.json());
